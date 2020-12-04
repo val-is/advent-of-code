f8c2491936400d45f0090a28b6015bd24a9abb21
val-is <vall_is@outlook.com>
2020-12-04 00:51:47 -0500

    AOC day 4 but less cringe

--- 2020/day4.py
+++ 2020/day4.py
@@ -1,117 +1,61 @@
 import re
 
-
 data = open('inputs/day4.txt', 'r').read().strip()
-portsRaw = data.split('\n\n')
+ports_raw = data.split('\n\n')
 
-types = [
-            "byr",
-            "iyr",
-            "eyr",
-            "hgt",
-            "hcl",
-            "ecl",
-            "pid",
-            "cid"
-        ]
+field_types = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
+
+def parse_port(raw):
+    fields = re.split(r'\n| ', raw)
+    port = {}
+    for f in fields:
+        t = f[:3]
+        if t not in field_types:
+            return port, False
+        port[t] = f[4:]
+    return port, True
 
 ports = []
-for i in portsRaw:
-    fields = re.split(r'\n| ', i)
-    struct = {}
-    for f in fields:
-        for t in types:
-            if f.startswith(t):
-                struct[t] = f[4:]
-    ports += [struct]
+for i in ports_raw:
+    port, valid = parse_port(i)
+    if not valid:
+        continue
+    ports += [port]
 
-def checkValidp1(port):
-    for t in types:
-        if t not in port.keys() and t != "cid":
+def check_fields_present(port):
+    for field in field_types:
+        if field not in port.keys() and field != "cid":
             return False
     return True
 
-vports = []
-for port in ports:
-    if checkValidp1(port):
-        vports += [port]
+vports = [port for port in ports if check_fields_present(port)]
 print(f"part 1: {len(vports)}")
 
-def getYear(yStr):
-    if not yStr.isnumeric():
-        return 0, False
-    return int(yStr), True
-
-def getValidHgt(hgtStr):
-    intPart = hgtStr[:-2]
-    units = hgtStr[-2:]
-    if not intPart.isnumeric():
+def check_height(height_raw):
+    unit = height_raw[-2:]
+    if unit not in ["cm", "in"]:
         return False
-    hgt = int(intPart)
-    if units == "cm":
-        if not( 150 <= hgt <= 193):
+    hgt = int(height_raw[:-2])
+    return {
+        "cm": lambda x: 150 <= x <= 193,
+        "in": lambda x: 59 <= x <= 76
+    }[unit](hgt)
+
+def validate_fields(port):
+    if not check_fields_present(port):
+        return False
+    for field in port:
+        if not {
+            "byr": lambda x: 1920 <= int(x) <= 2002,
+            "iyr": lambda x: 2010 <= int(x) <= 2020,
+            "eyr": lambda x: 2020 <= int(x) <= 2030,
+            "hgt": check_height,
+            "ecl": lambda x: x in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"],
+            "hcl": lambda x: re.match(r"^#[0-9a-f]{6}$", x) != None,
+            "pid": lambda x: re.match(r"^\d{9}$", x) != None
+        }.get(field, lambda x: True)(port[field]):
             return False
-    if units == "in":
-        if not (59 <= hgt <= 76):
-            return False
-    if units not in ["cm", "in"]:
-        return False
     return True
 
-def checkHair(clr):
-    if len(clr) != 7 or clr[0] != "#":
-        return False
-    if not re.match(r"#[0-9a-f]{6}", clr):
-        return False
-    return True
-
-def checkEye(eyeclr):
-    if eyeclr not in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]:
-        return False
-    return True
-
-def checkPid(pid):
-    if not pid.isnumeric():
-        return False
-    if len(pid) != 9 or pid[0] == "-":
-        return False
-    return True
-
-def checkValidp2(port):    
-    yr, valid = getYear(port["byr"])
-    if not valid:
-        return False
-    if yr < 1920 or yr > 2002:
-        return False
-    
-    yr, valid = getYear(port["iyr"])
-    if not valid:
-        return False
-    if yr < 2010 or yr > 2020:
-        return False
-
-    yr, valid = getYear(port["eyr"])
-    if not valid:
-        return False
-    if yr < 2020 or yr > 2030:
-        return False
-
-    if not getValidHgt(port["hgt"]):
-        return False
-    
-    if not checkEye(port["ecl"]):
-        return False
-    
-    if not checkHair(port["hcl"]):
-        return False
-
-    if not checkPid(port["pid"]):
-        return False
-
-    return True
-
-part2 = 0
-for port in vports:
-    if checkValidp2(port):
-        part2 += 1
-print(f"part 2: {part2}")
+vports = [port for port in vports if validate_fields(port)]
+print(f"part 2: {len(vports)}")
