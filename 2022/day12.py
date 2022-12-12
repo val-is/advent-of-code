5ac7e59bc1fbd008f5240f001f246588d7f7553d
val-is <vall_is@outlook.com>
2022-12-12 00:54:32 -0500

    day 12 2022, but without recursion

--- 2022/day12.py
+++ 2022/day12.py
@@ -35,34 +35,73 @@ def can_move_to(tiles, fro, to):
         return True
     return False
 
-def trav_recurse(tiles, cur_pos, cur_dist=0, best_dists={}, travelled=[]):
-    if cur_pos == end_pos:
-        return
-    if cur_pos in travelled:
-        return
-    adjs = get_adj(tiles, cur_pos)
-    adjs = sorted(adjs, key=lambda x: tiles[x])[::-1]
-    new_travelled = [*travelled] + [cur_pos]
-    for adj in adjs:
+def score_tile(pos):
+    return abs(pos[0] - end_pos[0]) + abs(pos[1] - end_pos[1])
+
+next_tiles = [start_pos]
+best_dists = {start_pos: -1}
+
+while len(next_tiles) > 0:
+    cur_pos = next_tiles.pop(0)
+    cur_dist = best_dists[cur_pos]
+    for adj in get_adj(tiles, cur_pos):
         if not can_move_to(tiles, cur_pos, adj):
             continue
+        dist = cur_dist + 1
+        if adj in best_dists and best_dists[adj] <= dist:
+            continue
+        best_dists[adj] = dist
+        if adj in next_tiles:
+            continue
+        next_tiles.append(adj)
 
-        if adj in best_dists:
-            if cur_dist >= best_dists[adj]:
-                continue
+    # next_tiles = sorted(next_tiles, key=score_tile)
+
+# def trav_recurse(tiles, cur_pos, cur_dist=0, best_dists={}, travelled=[]):
+#     if cur_pos == end_pos:
+#         return
+#     if cur_pos in travelled:
+#         return
+#     adjs = get_adj(tiles, cur_pos)
+#     adjs = sorted(adjs, key=lambda x: tiles[x])[::-1]
+#     new_travelled = [*travelled] + [cur_pos]
+#     for adj in adjs:
+#         if not can_move_to(tiles, cur_pos, adj):
+#             continue
+
+#         if adj in best_dists:
+#             if cur_dist >= best_dists[adj]:
+#                 continue
         
-        best_dists[adj] = cur_dist
-        trav_recurse(tiles, adj, cur_dist+1, best_dists, new_travelled)
-    return best_dists
+#         best_dists[adj] = cur_dist
+#         trav_recurse(tiles, adj, cur_dist+1, best_dists, new_travelled)
+#     return best_dists
 
-best_dists = {}
-trav_recurse(tiles, start_pos, 0, best_dists)
+# best_dists = {}
+# trav_recurse(tiles, start_pos, 0, best_dists)
 part1 = best_dists[end_pos]+1
 print(f"part 1: {part1}")
 
-for pos in tiles:
-    if tiles[pos] == 0:
-        trav_recurse(tiles, pos, 0, best_dists)
+next_tiles = [i for i in tiles if tiles[i] == 0]
+while len(next_tiles) > 0:
+    cur_pos = next_tiles.pop(0)
+    cur_dist = best_dists[cur_pos]
+    if tiles[cur_pos] == 0:
+        cur_dist = -1
+    for adj in get_adj(tiles, cur_pos):
+        if not can_move_to(tiles, cur_pos, adj):
+            continue
+        dist = cur_dist + 1
+        if adj in best_dists and best_dists[adj] <= dist:
+            continue
+        best_dists[adj] = dist
+        if adj in next_tiles:
+            continue
+        next_tiles.append(adj)
+
+# for pos in tiles:
+#     if tiles[pos] == 0:
+#         trav_recurse(tiles, pos, 0, best_dists)
 
 part2 = best_dists[end_pos]+1
 print(f"part 2: {part2}")
